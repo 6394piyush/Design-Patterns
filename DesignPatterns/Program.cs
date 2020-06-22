5beea9301d800b87f60991186f7948487f4aaac1
6394piyush <6394piyush@gmail.com>
2020-06-22 07:51:18 +0530

    Structural-Decorator

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -2,11 +2,47 @@
 
 namespace DesignPatterns
 {
+    class DecoratorPattern
+    {
+
+    }
+    interface IComponent
+    {
+        string Operation();        
+    }
+
+    class Component : IComponent
+    {
+        public string Operation()
+        {
+            return "I am Operation";
+        }
+    }
+
+    class DecoratorA : IComponent
+    {
+        
+            IComponent component;
+
+            public DecoratorA(IComponent c)
+            {
+                component = c;
+            }
+        public string Operation()
+        {
+            string s = component.Operation();
+            string st = s + " returned after modyfying from Decorator";
+            return st;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Design Patterns");
+            IComponent component = new Component();
+            Console.WriteLine(component.Operation());
+            Console.WriteLine(new DecoratorA(component).Operation());
         }
     }
 }
