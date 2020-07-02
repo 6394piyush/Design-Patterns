fc4ce1a15f0093c5d04a575e640749e320b77849
6394piyush <6394piyush@gmail.com>
2020-07-02 09:57:39 +0530

    Facade Pattern(Opaque)

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -3,40 +3,60 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+namespace FacadeLib{
+    internal class SubsystemA
+    {
+        internal string A1()
+        {
+            return "Subsystem A Method A1";
+        }
+        internal string A2()
+        {
+            return "Subsystem A Method A2";
+        }
+        internal string A3()
+        {
+            return "Subsystem A Method A3";
+        }
+    }
+    internal class SubsystemB
+    {
+        internal string B1()
+        {
+            return "Subsystem B Method B1";
+        }
+        internal string B2()
+        {
+            return "Subsystem B Method B2";
+        }
+    }
+    internal class SubsystemC
+    {
+        internal string C1()
+        {
+            return "Subsystem C Method C1";
+        }
+    }
+    public static class Facade 
+    {
+        static SubsystemA a = new SubsystemA();
+        static SubsystemB b = new SubsystemB();
+        static SubsystemC c = new SubsystemC();
+
+        public static void Operation1()
+        {
+            Console.WriteLine("Operation 1:"+a.A1()+b.B1()+c.C1());
+        }
+        public static void Operation2()
+        {
+            Console.WriteLine("Operation 2:" + a.A2() + b.B2() + a.A3());
+        }
+    }
+}
 
 namespace DesignPatterns
 {
-    class Adaptee
-    {   //existing way req are implemented
-        public double precise(double a,double b)
-        {
-            return a / b;
-        }
-    }
-
-    class Target
-    {
-        //new standard for requests
-        public string Estimate(int i)
-        {
-            return "estimate is " + (int)Math.Round(i / 3.0);
-        }
-    }
-    class Adaptor:Adaptee
-    {
-        public Func<int, string> Request;  //string Estimate(Int i) method signature
-        public Adaptor(Adaptee adaptee)
-        {
-            Request = delegate (int i)
-            {
-                return "Estimate based in Precision is " + (int)Math.Round(precise(i, 3));
-            };
-        }
-        public Adaptor(Target target)
-        {
-            Request = target.Estimate;
-        }
-    }
+    
    
    
 
@@ -45,15 +65,15 @@ namespace DesignPatterns
         
             static void Main(string[] args)
             {
-
             Console.WriteLine("Hello Design Patterns");
 
-            Adaptor adapter1 = new Adaptor(new Adaptee());
-             Console.WriteLine(adapter1.Request(5));
-            
-            Adaptor adapter2 = new Adaptor(new Target());
-            Console.WriteLine(adapter2.Request(5));
+            FacadeLib.Facade.Operation1();
+            FacadeLib.Facade.Operation2();
 
+            
+
+
+            
         }   
     }
 }
