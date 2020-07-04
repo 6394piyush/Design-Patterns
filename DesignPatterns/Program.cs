600dfae72096625677c17a928fabfa95d846d85b
6394piyush <6394piyush@gmail.com>
2020-07-04 08:19:48 +0530

    Prototype Pattern

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,79 +1,103 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 
-namespace FacadeLib{
-    internal class SubsystemA
-    {
-        internal string A1()
-        {
-            return "Subsystem A Method A1";
-        }
-        internal string A2()
-        {
-            return "Subsystem A Method A2";
-        }
-        internal string A3()
-        {
-            return "Subsystem A Method A3";
-        }
-    }
-    internal class SubsystemB
-    {
-        internal string B1()
-        {
-            return "Subsystem B Method B1";
-        }
-        internal string B2()
-        {
-            return "Subsystem B Method B2";
-        }
-    }
-    internal class SubsystemC
-    {
-        internal string C1()
-        {
-            return "Subsystem C Method C1";
-        }
-    }
-    public static class Facade 
-    {
-        static SubsystemA a = new SubsystemA();
-        static SubsystemB b = new SubsystemB();
-        static SubsystemC c = new SubsystemC();
-
-        public static void Operation1()
-        {
-            Console.WriteLine("Operation 1:"+a.A1()+b.B1()+c.C1());
-        }
-        public static void Operation2()
-        {
-            Console.WriteLine("Operation 2:" + a.A2() + b.B2() + a.A3());
-        }
-    }
-}
 
 namespace DesignPatterns
 {
-    
-   
-   
+    public abstract class IPrototype<T>
+    {
+        //shallow Copy
+        public T Clone()
+        {
+            return (T)this.MemberwiseClone();
+        }
 
+        //Deep Copy
+        public T DeepCopy()
+        {
+            MemoryStream ms = new MemoryStream();
+            BinaryFormatter bf = new BinaryFormatter();
+
+            bf.Serialize(ms, this);
+            ms.Seek(0, SeekOrigin.Begin);
+            T copy = (T)bf.Deserialize(ms);
+            ms.Close();
+            return copy;
+        }
+    }
+    [Serializable()]
+    class Prototype:IPrototype<Prototype>
+    {
+        public string Country { get; set; }
+        public string Capital { get; set; }
+        public DeeperData Language { get; set; }
+
+        public Prototype(string ctry,string cap,string lang)
+        {
+            Country = ctry;
+            Capital = cap;
+            Language = new DeeperData(lang);
+        }
+
+        public override string ToString()
+        {
+             return Country + "\t\t" + Capital + "\t\t->" + Language;
+        }
+    }
+
+    [Serializable()]
+    class DeeperData
+    {
+        public string data { get; set; }
+        public DeeperData(string s)
+        {
+            data = s;
+        }
+        public override string ToString()
+        {
+            return data;
+        }
+    }
+
+    class PrototypeManager:IPrototype<Prototype>
+    {
+        public Dictionary<string, Prototype> prototypes = new Dictionary<string, Prototype>
+        {
+            { "Germany",new Prototype("Germany","Berlin","Germen") },
+            { "Italy",new Prototype("Italy","Rome","Italian") },
+            { "Australia",new Prototype("Australia","Canberra","English") }
+        };
+    }
+
+    class PrototypeClient:IPrototype<Prototype>
+    {
+        static void Report(string s,Prototype p,Prototype q)
+        {
+            Console.WriteLine("Prototye "+p+"\nClone "+q);
+        }
+    }
+        
     class Program
     {
-        
-            static void Main(string[] args)
-            {
+
+        static void Main(string[] args)
+        {
             Console.WriteLine("Hello Design Patterns");
-
-            FacadeLib.Facade.Operation1();
-            FacadeLib.Facade.Operation2();
-
-            
+            PrototypeManager pm = new PrototypeManager();
+            Prototype c2, c3;
+            c2 = pm.prototypes["Australia"].Clone();
+            Console.WriteLine("Clonning Australia "+"Prototye " + pm.prototypes["Australia"] + "\nClone " + c2);
+            c2.Capital = "Sydney";
+            Console.WriteLine("Altering Australia shallow state " + "Prototye " + pm.prototypes["Australia"] + "\nClone " + c2);
+            c2.Language.data = "Chinese";
+            Console.WriteLine("Altering Australia Deep state " + "Prototye " + pm.prototypes["Australia"] + "\nClone " + c2);
+        }
 
 
             
         }   
     }
-}
