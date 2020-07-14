69fb1dc2db3f90e9d4d57f508cefd4dc84e10cb4
6394piyush <6394piyush@gmail.com>
2020-07-14 16:11:34 +0530

    Mediator Pattern

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -10,33 +10,66 @@ using System.Text;
 namespace DesignPatterns
 {
 
-    class MonthIterator : IEnumerable
-    {
-        string[] months = { "January", "February","March","April","May","June","July","August","September","October","November","December" };
-        public IEnumerator GetEnumerator()
-        {
-            foreach(string item in months)
-            {
-                yield return item;
-            }
-        }
-    }
+
 
 
     class Program
+    {
+        class Mediator
         {
+            public delegate void Callback(string msg, string from);
+            Callback respond;
+            public void SignOn(Callback method)
+            {
+                respond += method;
+            }
+            public void Block(Callback method)
+            {
+                respond -= method;
+            }
+            public void Unblock(Callback method)
+            {
+                respond += method;
+            }
+            public void Send(string msg, string from)
+            {
+                respond(msg, from);
+                Console.WriteLine();
+            }
+        }
+
+        class Colleague
+        {
+            Mediator mediator;
+            public string name;
+            public Colleague(Mediator med,string name)
+            {
+                mediator = med;
+                mediator.SignOn(Recieve);
+                this.name = name;
+            }
+            public virtual void Recieve(string msg,string from)
+            {
+                Console.WriteLine(name+" Recieved From "+from+" -> "+msg);
+            }
+            public void Send(string msg)
+            {
+                Console.WriteLine("Send from "+name+" -> "+msg);
+                mediator.Send(msg,name);
+            }
+        }
 
 
         static void Main(string[] args)
             {
                 Console.WriteLine("Hello Design Patterns");
-                MonthIterator month = new MonthIterator();
-                foreach(string n in month)
-                {
-                    Console.WriteLine(n+" ");
-                    Console.WriteLine();
-                }
+            Mediator m = new Mediator();
+            Colleague cl = new Colleague(m,"Piyush");
+            cl.Send("Hello This is sent from delegates");
+            cl.Send("waiting for more messages");
+
             }
+                
 
 
 
